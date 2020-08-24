105101646d61d358d7a1c405536e353be5e87b0f
Kookiat Suetrong <beer@Kookiats-iMac.local>
2020-08-24 15:46:55 +0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -17,32 +17,21 @@ create table forms(
 );
 
 create table elements(
-    id        int unique not null auto_increment,
-    form_id   int not null,
-    title     varchar(200) not null default '',
-    type      varchar(200) not null
+    id          int unique not null auto_increment,
+    form_id     int not null,
+    title       varchar(200) not null default '',
+    type        varchar(200) not null,
+    placeholder varchar(200),
+    min         int default  0,
+    max         int default 10,
+    value       int,
+    step        int,
+    detail      varchar(200),
+    status      varchar(200) default 'active'
 );
-alter table elements add placeholder varchar(200);
-alter table elements add min int default 0;
-alter table elements add max int default 10;
-alter table elements add value int;
-alter table elements add step int;
-alter table elements add detail varchar(200);
-alter table elements add status varchar(200) default 'active';
 
 insert into users(email,password,full_name)
     values('user@email.com', sha2('user', 512), 'Administrator');
-insert into forms(user_id, title)
-    values(1, 'Form 1');
-
-insert into elements(form_id, title, type)
-    values(1, 'Element 1', 'text');
-insert into elements(form_id, title, type)
-    values(1, 'Element 2', 'range');
-insert into elements(form_id, title, type)
-    values(1, 'Element 3', 'text');
-insert into elements(form_id, title, type)
-    values(1, 'Overall', 'range');
 
 create table form_values(
     id         int unique not null auto_increment,
